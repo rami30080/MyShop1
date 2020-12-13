21e51f15064bed515c82a647ea9e1a903e0add5b
Rami <ramiayoub12123@gmail.com>
2020-12-13 05:38:47 +0200

    init

--- index.js
+++ index.js
@@ -1,45 +1,83 @@
-const express = require('express');
+const express = require('express')
 const app = express();
+
 var bodyParser = require('body-parser')
-const jwt = require("jsonwebtoken");
-const cookies = require("cookie-parser");
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({ 
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
 
-const port=process.env.PORT ||4000;
-
-const TaskModel = require('./schemas/TaskSchema');
-
-var secret = 'abcdefghujklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%^&*()_+';
-module.exports = secret;
-
-
-app.use(express.static('public'))
-
-app.use(cookies());
-
-const url = "mongodb+srv://nimer:N1N1N1N1@cluster0.tejcy.mongodb.net/server";
+app.use(express.static('public'));
 
 const mongoose = require('mongoose');
+const e = require('express');
+
+const port=process.env.PORT ||4000;
+
+const url = "mongodb+srv://rami30080:mxzmxz123@cluster0.halwb.mongodb.net/tasks";
+
+
 mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
+const users = mongoose.model('users',{
+    email:String,
+    password:String,
+    fname:String,
+    lname:String,
+    img:String,
+    role:String
+  });
+  
+  //users.insertMany({email:"rami@hotmail.com",password:"123",name:"Rami",img:"pic",role:"master"})
 
+app.post('/api/login', (req, res) => {
+    const { email, password } = req.body
+    console.log(email)
+    users.find({ email: email }).then(doc => {
+        if (doc.length > 0) {
+            users.find({ email: email, password: password }).then(docs => {
+                if (docs.length > 0) {
+                    
+                    res.send({ success: true, error: null, info: { role: docs[0].role } })
+                }
+                else {
+                    res.send({ success: false, error: "Password Doesnt match Email", info: null })
+                }
+            })
+        }
+        else{
+        res.send({ success: false, error: "Email not found", info: null })
+    }
+    })
+            
+})
 
-const userRouter = require('./routing/user');
-app.use("/api/users", userRouter);
-
-const adminRouter = require('./routing/admin');
-app.use("/api/admin", adminRouter);
-
-
+app.post('/api/SignUp', (req, res) => {
+    const { email, password,fname,lname,role} = req.body
+    console.log(email)
+    users.find({ email: email }).then(doc => {
+        if (doc.length > 0) {
+            res.send({ success: false, error: "Email Already In Use", info: null })
+        }
+        else{
+        users.insertMany({email:email,
+            password:password,
+            fname:fname,
+            lname:lname,
+            img:null,
+            role:role}).then(docs=>{
+                if(docs.length>0){
+                    res.send({ success: true, error: null, info: null })
+                }
+            })
+    }
+    })
+            
+})
 
 app.listen(port, () => {
     console.log("App is Listening to port:",port)
 })
-
-
---
+++ tasks/src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Login from "./view/pages/Login/Login";
+import forgetPassword from "./view/pages/ForgetPassword/ForgetPassword";
+import SignUp from "./view/pages/SignUp/SignUp";
+
+import './App.css';
+
+function App() {
+  return (
+       <Router>
+      <div className="app">
+        <div className="mainPage">
+          <Switch>
+            <Route exact={true} path="/">
+              <Login />
+            </Route>
+            <Route path="/forgotPassword">
+              <forgetPassword/>
+            </Route>
+            <Route path="/SignUp">
+              <SignUp/>
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
